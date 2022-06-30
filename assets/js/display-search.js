91f8c2cf14898c739ccdb7b57304f7cb93c801b7
Deejerz88 <djmcmillan93@gmail.com>
2022-06-30 18:02:29 -0400

    small adjustments

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -35,89 +35,88 @@ let container = document.getElementById("result-content");
 
 fetch(url).then((response) => {
   if (response.ok) {
-  }
+    response.json().then((data) => {
+      clearInterval(loadingInt);
+      container.innerHTML = "";
+      console.log(data.results, data);
+      let results = data.results;
 
-  response.json().then((data) => {
-    clearInterval(loadingInt);
-    container.textContent = "";
-    console.log(data.results, data);
-    let results = data.results;
+      let lastImgUrl = "";
+      results.forEach((row) => {
+        console.log(row);
+        // title*, img_url(array)*, date*, subject(array)*, description*, id
 
-    let lastImgUrl = "";
-    results.forEach((row) => {
-      console.log(row);
-      // title*, img_url(array)*, date*, subject(array)*, description*, id
+        //create card
+        let cardEl = document.createElement("div");
+        cardEl.id = "card";
+        cardEl.classList.add("p-3", "bg-secondary", "mb-3", "rounded");
 
-      //create card
-      let cardEl = document.createElement("div");
-      cardEl.id = "card";
-      cardEl.classList.add("p-3", "bg-secondary", "mb-3", "rounded");
+        //create title and append
+        let title = document.createElement("h1");
+        title.textContent = row.title;
+        cardEl.append(title);
 
-      //create title and append
-      let title = document.createElement("h1");
-      title.textContent = row.title;
-      cardEl.append(title);
+        //create image element & add to card
+        let imgEl = document.createElement("img");
 
-      //create image element & add to card
-      let imgEl = document.createElement("img");
+        //store last full image_url
+        lastImgUrl = !!row.image_url[0] ? row.image_url[0] : lastImgUrl;
 
-      //store last full image_url
-      lastImgUrl = !!row.image_url[0] ? row.image_url[0] : lastImgUrl;
+        //set img src to last full image_url
+        let imgURL = lastImgUrl;
+        imgEl.src =
+          imgURL ===
+            "https://www.loc.gov/static/images/original-format/group-of-images.svg" ||
+          !imgURL
+            ? "https://pbs.twimg.com/media/E1VGfdXVkAAZMml.jpg"
+            : imgURL;
+        imgEl.width = 250;
+        imgEl.height = 200;
+        cardEl.append(imgEl);
 
-      //set img src to last full image_url
-      let imgURL = lastImgUrl;
-      imgEl.src =
-        imgURL ===
-          "https://www.loc.gov/static/images/original-format/group-of-images.svg" ||
-        !imgURL
-          ? "https://pbs.twimg.com/media/E1VGfdXVkAAZMml.jpg"
-          : imgURL;
-      imgEl.width = 250;
-      imgEl.height = 200;
-      cardEl.append(imgEl);
+        //create date & append
+        let date = document.createElement("p");
+        let rowDate = row.date;
+        date.textContent = `Date: ${rowDate}`;
+        cardEl.append(date);
 
-      //create date & append
-      let date = document.createElement("p");
-      let rowDate = row.date;
-      date.textContent = `Date: ${rowDate}`;
-      cardEl.append(date);
-
-      //subject & append
-      let subjectArr = row.subject;
-      let subjects = "";
-      if (!!subjectArr) {
-        let max = subjectArr.length < 7 ? subjectArr.length : 7;
-        console.log(max);
-        for (let i = 0; i < max; i++) {
-          subjects += subjectArr[i];
-          console.log(i < max - 1);
-          if (i < max - 1) subjects += ", ";
+        //subject & append
+        let subjectArr = row.subject;
+        let subjects = "";
+        if (!!subjectArr) {
+          let max = subjectArr.length < 7 ? subjectArr.length : 7;
+          console.log(max);
+          for (let i = 0; i < max; i++) {
+            subjects += subjectArr[i];
+            console.log(i < max - 1);
+            if (i < max - 1) subjects += ", ";
+          }
         }
-      }
-      let subjectEl = document.createElement("p");
-      subjectEl.textContent = `Subjects: ${subjects}`;
+        let subjectEl = document.createElement("p");
+        subjectEl.textContent = `Subjects: ${subjects}`;
 
-      cardEl.append(subjectEl);
+        cardEl.append(subjectEl);
 
-      //create desc & append
+        //create desc & append
 
-      let descriptionEl = document.createElement("p");
-      let rowDescription = row.description;
-      descriptionEl.textContent = `Description: ${rowDescription}`;
+        let descriptionEl = document.createElement("p");
+        let rowDescription = row.description;
+        descriptionEl.textContent = `Description: ${rowDescription}`;
 
-      cardEl.append(descriptionEl);
+        cardEl.append(descriptionEl);
 
-      let btnEl = document.createElement("button");
-      btnEl.classList.add("btn", "btn-danger");
-      btnEl.textContent = "Read More!";
+        let btnEl = document.createElement("button");
+        btnEl.classList.add("btn", "btn-danger");
+        btnEl.textContent = "Read More!";
 
-      let anchor = document.createElement("a");
-      anchor.href = row.id;
+        let anchor = document.createElement("a");
+        anchor.href = row.id;
 
-      anchor.append(btnEl);
-      cardEl.append(anchor);
+        anchor.append(btnEl);
+        cardEl.append(anchor);
 
-      container.append(cardEl);
+        container.append(cardEl);
+      });
     });
-  });
+  }
 });
